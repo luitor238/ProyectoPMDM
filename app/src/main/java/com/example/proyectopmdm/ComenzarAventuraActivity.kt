be19a52fc4eded20dc215e0f75a9d7a5c2cfa5e3
luitor238 <luitor238@alu.centroafuera.es>
2023-12-14 18:11:39 +0100

    Añado codigo kotlin en MainActivity.kt para crear un objeto personaje Añado codigo kotlin en ComenzarAventuraActivity.kt para asociar atributos del personaje a los textView

--- app/src/main/java/com/example/proyectopmdm/ComenzarAventuraActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/ComenzarAventuraActivity.kt
@@ -4,14 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class ComenzarAventuraActivity : AppCompatActivity() {
 
     private lateinit var btnVolver: Button
     private lateinit var btnJugar: Button
+
+    private lateinit var personaje: Personaje
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comenzar_aventura)
+
+        personaje = intent.getSerializableExtra("Personaje") as Personaje
+
+        val nombre = findViewById<TextView>(R.id.varNombre)
+        val expNivel = findViewById<TextView>(R.id.textExpNivel)
+        val clase = findViewById<TextView>(R.id.varClase)
+        val raza = findViewById<TextView>(R.id.varRaza)
+        val estadoVital = findViewById<TextView>(R.id.varEstadoVital)
+        val salud = findViewById<TextView>(R.id.varSalud)
+        val ataque = findViewById<TextView>(R.id.varAtaque)
+        val defensa = findViewById<TextView>(R.id.varDefensa)
+        val suerte = findViewById<TextView>(R.id.varSuerte)
+
+        nombre.text = personaje.getNombre()
+        expNivel.text = "${personaje.getExperiencia()}/${personaje.getNivel()}"
+        clase.text = personaje.getClase().toString()
+        raza.text = personaje.getRaza().toString()
+        estadoVital.text = personaje.getEstadoVital().toString()
+        salud.text = personaje.getSalud().toString()
+        ataque.text = personaje.getAtaque().toString()
+        defensa.text = personaje.getDefensa().toString()
+        suerte.text = personaje.getSuerte().toString()
+
 
         btnVolver=findViewById(R.id.btnVolverCrearPersonaje)
         btnJugar=findViewById(R.id.btnJugar)
--- app/src/main/java/com/example/proyectopmdm/MainActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Button
@@ -17,39 +18,71 @@ class MainActivity : AppCompatActivity() {
     private lateinit var spnRaza: Spinner
     private lateinit var spnEstVital: Spinner
 
-    private lateinit var nombre: EditText
+    private lateinit var nickname: EditText
     private lateinit var btnAplicar: Button
     private lateinit var btnCrear: Button
     private lateinit var btnVolver: Button
+    private lateinit var textViewError: TextView
 
-    lateinit var textViewError: TextView
-
+    private lateinit var personaje: Personaje
+    private lateinit var nombre: String
+    private lateinit var clase: String
+    private lateinit var raza: String
+    private lateinit var estadoVital: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Espiner desplegables
+        // Asignacion del nickname a la variable nombre
+
+        nickname = findViewById<EditText>(R.id.editTextNombre)
+        nombre = nickname.text.toString()
+
+        // Spinners de seleccion de clase, raza y estado vital y asignacion a sus respectivas variables
 
         spnClase = findViewById(R.id.spnClase)
         ArrayAdapter.createFromResource(this, R.array.Clase, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spnClase.adapter = adapter
         }
+        spnClase.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                clase = resources.getStringArray(R.array.Clase)[position].toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
         spnRaza = findViewById(R.id.spnRaza)
         ArrayAdapter.createFromResource(this, R.array.Raza, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spnRaza.adapter = adapter
+        }
+        spnRaza.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                raza = resources.getStringArray(R.array.Raza)[position].toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
         }
         spnEstVital = findViewById(R.id.spnEstadoVital)
         ArrayAdapter.createFromResource(this, R.array.EstadoVital, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spnEstVital.adapter = adapter
         }
+        spnEstVital.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                estadoVital = resources.getStringArray(R.array.EstadoVital)[position].toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
 
-        // Boton de aplicacion y creacion
+            }
+        }
 
-        nombre = findViewById(R.id.editTextNombre)
+        // Botones de aplicacion y creacion
+
         btnAplicar = findViewById(R.id.btnAplicar)
         btnCrear = findViewById(R.id.btnCrear)
         btnVolver = findViewById(R.id.btnVolver)
@@ -61,7 +94,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clickBotonAplicar() {
-        val texto = nombre.text.toString().trim()
+        val texto = nickname.text.toString().trim()
 
         if (texto.isNotEmpty()) {
             // Si el nombre está relleno, oculta el botón "Aplicar" muestra el botón "Crear"
@@ -74,8 +107,11 @@ class MainActivity : AppCompatActivity() {
 
             // Funcion del boton crear
             btnCrear.setOnClickListener {
+                personaje = Personaje(nombre,Personaje.Raza.valueOf(raza.toUpperCase()),Personaje.Clase.valueOf(clase.toUpperCase()),Personaje.EstadoVital.valueOf(estadoVital.toUpperCase()))
                 val intent = Intent(this@MainActivity, ComenzarAventuraActivity::class.java)
+                intent.putExtra("Personaje",personaje)
                 startActivity(intent)
+
 
             }
             btnVolver.setOnClickListener{
