ed05b017201083be3b468b2692d0770a2684a3d1
luitor238 <luitor238@alu.centroafuera.es>
2024-01-23 19:52:51 +0100

    xxx

--- app/src/main/java/com/example/proyectopmdm/eventos/ObjetoActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/eventos/ObjetoActivity.kt
@@ -1,11 +1,13 @@
 package com.example.proyectopmdm.eventos
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.text.parseAsHtml
 import com.example.proyectopmdm.Articulo
@@ -16,15 +18,32 @@ import kotlin.random.Random
 
 class ObjetoActivity : AppCompatActivity() {
 
+    private lateinit var nombre1: TextView
+    private lateinit var tipoArticulo: TextView
     private lateinit var imagen: ImageView
+    private lateinit var peso1: TextView
+    private lateinit var precio: TextView
+    private lateinit var aumentoAtaque: TextView
+    private lateinit var aumentoDefensa: TextView
+    private lateinit var aumentoVida: TextView
     private lateinit var btnRecoger: Button
     private lateinit var btnContinuar: Button
 
     private val TAG = "LoginActivity"
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_objeto)
 
+        //TextView's
+        nombre1 = findViewById(R.id.textViewNombre)
+        tipoArticulo = findViewById(R.id.textViewTipoArticulo)
+        peso1 = findViewById(R.id.textViewPeso)
+        precio = findViewById(R.id.textViewPrecio)
+        aumentoAtaque = findViewById(R.id.textViewAumentoAtaque)
+        aumentoDefensa = findViewById(R.id.textViewAumentoDefensa)
+        aumentoVida = findViewById(R.id.textViewAumentoVida)
+        //Botones
         btnRecoger = findViewById(R.id.btnRecoger)
         btnContinuar = findViewById(R.id.btnContinuar)
 
@@ -34,7 +53,15 @@ class ObjetoActivity : AppCompatActivity() {
 
         var articulo = Articulo(nombre,peso)
 
+
+        nombre1.text = articulo.getNombre().toString()
+        tipoArticulo.text = articulo.getTipoArticulo().toString()
         imagen.setImageResource(articulo.getImagen().toInt())
+        peso1.text = articulo.getPeso().toString()
+        precio.text = articulo.getPrecio().toString()
+        aumentoAtaque.text = articulo.getAumentoAtaque().toString()
+        aumentoDefensa.text = articulo.getAumentoDefensa().toString()
+        aumentoVida.text = articulo.getAumentoVida().toString()
 
 
 
