b703b5aafbb89eb44c4b3c2d1f0535ffd8d2108b
luitor238 <luistorgacastro@gmail.com>
2024-02-09 10:35:23 +0100

    xxx

--- app/src/main/java/com/example/proyectopmdm/LoginActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/LoginActivity.kt
@@ -1,11 +1,90 @@
 package com.example.proyectopmdm
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    //VARIABLES
+    private lateinit var Email : EditText
+    private lateinit var Password : EditText
+    private lateinit var btnIniciarSesion: Button
+    private lateinit var btnCrearCuenta: Button
+    private lateinit var textViewWarning: TextView
+    private lateinit var auth: FirebaseAuth
+    //VARIABLE SISTEMA LOG
+    private val TAG = "LoginActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Log.d(TAG, "onCreate: La actividad está siendo creada")
+        //CREACION DE LA VISTA
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //ASIGNACION DE VARIABLES CON ELEMENTOS LAYOUT
+        Log.d(TAG, "ASIGNACION DE VARIABLES CON ELEMENTOS LAYOUT")
+
+
+        Email = findViewById(R.id.editTextEmail)
+        Password = findViewById(R.id.editTextPassword)
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion)
+        btnCrearCuenta = findViewById(R.id.btnCrearCuenta)
+        textViewWarning = findViewById(R.id.textViewWarning)
+
+
+
+        try {
+            btnIniciarSesion.setOnClickListener{
+                if (Email.text.isNotEmpty() && Password.text.isNotEmpty()){
+                    auth.signInWithEmailAndPassword(Email.text.toString(), Password.text.toString()).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Autenticacion del ususario Correcta")
+                            //val user = auth.currentUser
+                            val intent = Intent(this, VerPersonajeActivity::class.java)
+                            intent.putExtra("email", Email.text.toString())
+                            startActivity(intent)
+
+
+                        } else {
+                            val builder = AlertDialog.Builder(this)
+                            builder.setTitle("Error")
+                            builder.setMessage("Se ha producido un error en la autenticacion del ususario")
+                            builder.setPositiveButton("Aceptar",null)
+                            val dialog: AlertDialog = builder.create()
+                            dialog.show()
+                        }
+                    }
+
+                }else{ Log.d(TAG, "Debes rellenar los campos") }
+
+            }
+        } catch (e: Exception) {
+            Log.d(TAG, "Error en la autentificacion del usuario")
+        }
+
+
+        try {
+            btnCrearCuenta.setOnClickListener(View.OnClickListener {
+                val intent = Intent(this, SingInActivity::class.java)
+                startActivity(intent)
+            })
+
+        } catch (e: Exception) {
+            Log.d(TAG, "Usuario No Creado Correctamente")
+        }
     }
 }
--- app/src/main/java/com/example/proyectopmdm/SingInActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/SingInActivity.kt
@@ -1,11 +1,75 @@
 package com.example.proyectopmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class SingInActivity : AppCompatActivity() {
+
+    private lateinit var Email : EditText
+    private lateinit var Password : EditText
+    private lateinit var RepeatPassword : EditText
+    private lateinit var btnCrearCuenta: Button
+    private lateinit var textViewWarning: TextView
+    private lateinit var auth: FirebaseAuth
+    //VARIABLE SISTEMA LOG
+    private val TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_in)
+
+        Email = findViewById(R.id.editTextEmail)
+        Password = findViewById(R.id.editTextPassword)
+        RepeatPassword = findViewById(R.id.editTextRepeatPassword)
+        btnCrearCuenta = findViewById(R.id.btnCrear)
+        textViewWarning = findViewById(R.id.textViewWarning)
+        auth= Firebase.auth
+
+        try {
+            btnCrearCuenta.setOnClickListener{
+
+                if (Email.text.isNotEmpty() && Password.text.isNotEmpty() && RepeatPassword.text.isNotEmpty()){
+
+                    if(Password.text==RepeatPassword.text) {
+                        crearUsuario()
+                    }else{
+                        textViewWarning.text = "La contraseña no es igual"
+                        textViewWarning.visibility = View.VISIBLE
+                        Log.d(TAG, "La contraseña no es igual")
+                    }
+
+                } else{
+                    textViewWarning.text = "Debes rellenar todos los campos"
+                    textViewWarning.visibility = View.VISIBLE
+                    Log.d(TAG, "Debes rellenar todos los campos")
+                }
+            }
+
+        } catch (e: Exception) {
+            Log.d(TAG, "Error no esperado")
+        }
+    }
+
+
+    //Funcion CREAR USUARIO
+    fun crearUsuario(){
+        auth.createUserWithEmailAndPassword(Email.text.toString(), Password.text.toString()).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "Usuario Creado Correctamente")
+
+                val intent = Intent(this, VerPersonajeActivity::class.java)
+                startActivity(intent)
+            } else {
+                textViewWarning.text = "Usuario No Encontrado"
+                textViewWarning.visibility = View.VISIBLE
+                Log.d(TAG, "Usuario No Creado Correctamente")
+            }
+        }
     }
 }
