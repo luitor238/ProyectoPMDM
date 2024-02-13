ff2650952af77fdc15d1ac7fb09170a8942e494e
MarcoGarciaa <margar306@alu.centroafuera.es>
2024-02-13 23:13:46 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/proyectopmdm/LoginActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyectopmdm
 
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +9,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
