f54281fe83ad0d7a04288595d97abd515ed036c0
luitor238 <luitor238@alu.centroafuera.es>
2024-02-18 15:21:48 +0100

    xxx

--- app/src/main/java/com/example/proyectopmdm/ComunicacionActivity.kt
+++ app/src/main/java/com/example/proyectopmdm/ComunicacionActivity.kt
@@ -1,9 +1,11 @@
 package com.example.proyectopmdm
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectopmdm.GlobalVariables.personaje
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,8 +15,11 @@ class ComunicacionActivity : AppCompatActivity() {
 
     private lateinit var EditTextInput: EditText
     private lateinit var btnEnviar: Button
+    private lateinit var opcion1: Button
+    private lateinit var opcion2: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MessageAdapter
+    private lateinit var btnVolver: ImageButton
 
 
     @SuppressLint("MissingInflatedId")
@@ -26,6 +31,7 @@ class ComunicacionActivity : AppCompatActivity() {
         EditTextInput = findViewById(R.id.EditTextInput)
         btnEnviar = findViewById(R.id.btnEnviar)
         recyclerView = findViewById(R.id.recyclerView)
+        btnVolver = findViewById(R.id.btnVolver)
 
         adapter = MessageAdapter()
         recyclerView.adapter = adapter
@@ -33,8 +39,30 @@ class ComunicacionActivity : AppCompatActivity() {
 
         val instancia = MessageAdapter()
 
+        // Boton volver al menu
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, DadoActivity::class.java)
+            startActivity(intent)
+        }
+
         btnEnviar.setOnClickListener {
             val message = EditTextInput.text.toString().trim()
+            if (message.isNotEmpty()) {
+                instancia.sendMessage(message)
+                EditTextInput.text.clear()
+            }
+        }
+
+        opcion1.setOnClickListener {
+            val message = "¿Como estas?"
+            if (message.isNotEmpty()) {
+                instancia.sendMessage(message)
+                EditTextInput.text.clear()
+            }
+        }
+
+        opcion2.setOnClickListener {
+            val message = "¿Tienes algo equipado?"
             if (message.isNotEmpty()) {
                 instancia.sendMessage(message)
                 EditTextInput.text.clear()
